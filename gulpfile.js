2b61e908f6adfb6bb64f833a0cdc34d190e4e05d
Adam Williams <adam@whitesunrise.com>
2016-07-25 17:14:22 +0000

    Start Bower

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,17 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.styles([
+      '../../../bower_components/bootswatch-dist/css/bootstrap.css',
+      '../../../bower_components/components-font-awesome/css/font-awesome.css'
+    ],'public/css/app.css');
+
+    mix.scripts([
+      '../../../bower_components/jquery/dist/jquery.js',
+      '../../../bower_components/bootstrap/dist/js/bootstrap.js'
+    ], 'public/js/app.js');
+
+    mix.copy('bower_components/components-font-awesome/fonts', 'public/build/fonts');
+
+    mix.version(["css/app.css", "js/app.js"]);
 });
