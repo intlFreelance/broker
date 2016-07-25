190f6425f2f24a28f6740af27157a8b30457127b
Adam Williams <adam@whitesunrise.com>
2016-07-25 17:57:12 +0000

    Setup Entrust

--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Entrust Providers
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
     ],
 
     /*
@@ -201,7 +204,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
     ],
 
 ];
