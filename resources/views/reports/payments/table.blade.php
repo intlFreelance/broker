e69d245c9e7e56208fd6c443cfe467506b9d4470
Adam Williams <adam@whitesunrise.com>
2016-11-17 15:32:04 +0000

    For Demo

--- resources/views/reports/payments/table.blade.php
+++ resources/views/reports/payments/table.blade.php
@@ -4,21 +4,29 @@
         <th>Producer</th>
         <th>Payment Date</th>
         <th>Amount</th>
-        <th colspan="3" class="text-right">Action</th>
+        <th class="text-right">Action</th>
     </thead>
     <tbody>
+      <?php $total_payments = 0; ?>
       @foreach($payments as $payment)
         <tr>
             <td>{{ $payment->client->name}}</td>
             <td>{{ $payment->producer->last_name}}, {{ $payment->producer->first_name}}</td>
             <td>{{ date('m/d/Y',strtotime($payment->payment_date)) }}</td>
-            <td>${{ $payment->amount}}</td>
+            <td>${{ $payment->amount }}</td>
             <td class="text-right">
                 <div class='btn-group'>
                     <a href="#" class='btn btn-warning btn-xs'><i class="glyphicon glyphicon-edit"></i> Edit Payment</a>
                 </div>
             </td>
         </tr>
+        <?php $total_payments += $payment->amount; ?>
       @endforeach
+      <tr>
+          <td colspan="3"  class="text-right"><b>Total:</b></td>
+          <td>${{ $total_payments }}</td>
+          <td class="text-right">
+          </td>
+      </tr>
     </tbody>
 </table>
--- resources/views/reports/tracking/table.blade.php
+++ resources/views/reports/tracking/table.blade.php
@@ -8,6 +8,11 @@
         <th>Payment Date</th>
     </thead>
     <tbody>
+      <?php
+        $expected_total = 0;
+        $paid_total = 0;
+      ?>
+
     @foreach($contract_payments as $contract_payment)
       <?php
       if(isset($contract_payment->contract->payments->last()->amount)){
@@ -25,7 +30,13 @@
           <td>{{ isset($contract_payment->contract->client->name)?$contract_payment->contract->client->name:'' }}</td>
           <td>{{$contract_payment->producer->last_name}}, {{$contract_payment->producer->first_name}}</td>
           <td>${{$contract_payment->amount}}</td>
+          <?php $expected_total += $contract_payment->amount; ?>
           <td>{{isset($contract_payment->contract->payments->last()->amount)?'$'.$contract_payment->contract->payments->last()->amount:'NA'}}</td>
+          <?php
+          if(isset($contract_payment->contract->payments->last()->amount)){
+            $paid_total += $contract_payment->contract->payments->last()->amount;
+          }
+          ?>
           <td>
             <?php
               if(isset($contract_payment->contract->payments->last()->amount)){
@@ -34,7 +45,7 @@
                 }elseif($contract_payment->contract->payments->last()->amount < $contract_payment->amount){
                   echo '<b style="color:#F00;">-$'.($contract_payment->amount-$contract_payment->contract->payments->last()->amount).'</b>';
                 }else{
-                  echo '<b>$'.($contract_payment->amount-$contract_payment->contract->payments->last()->amount).'</b>';
+                  echo '<b>$'.($contract_payment->contract->payments->last()->amount-$contract_payment->amount).'</b>';
                 }
               }else{
                 echo '<b style="color:#F00;">-$'.$contract_payment->amount.'</b>';
@@ -44,5 +55,12 @@
           <td>{{isset($contract_payment->contract->payments->last()->payment_date)?date('m/d/Y',strtotime($contract_payment->contract->payments->last()->payment_date)):'NA'}}</td>
       </tr>
     @endforeach
+    <tr>
+        <td colspan="2" class="text-right"><b>Total:</b></td>
+        <td>${{ $expected_total }}</td>
+        <td>${{ $paid_total }}</td>
+        <td class="text-right">
+        </td>
+    </tr>
   </tbody>
 </table>
